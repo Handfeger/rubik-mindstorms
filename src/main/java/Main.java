23c9e181cacf6b78d3a956e88aaaa1a5be3d1117
Michel Vielmetter <mvielmetter@rsteinmann.de>
2015-05-14 12:38:44 +0200

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import lejos.hardware.lcd.LCD;
+
+/**
+ * ╔================================ Main ====================================
+ * ║
+ * ║ Create Date: 14.05.2015
+ * ║
+ * ║ Author(s): Michel Vielmetter <contact@michelvielmetter.de>
+ * ║ Copyright: Michel Vielmetter 2015
+ * ║ Licence:   MIT License
+ * ║
+ * ╠================================ Main ====================================
+ * ║
+ * ║ Package:   PACKAGE_NAME
+ * ║
+ * ╚================================ Main ====================================
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        LCD.clear();
+    }
+}
